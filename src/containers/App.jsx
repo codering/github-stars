d7f2ddf077284f6e1ab575fd4442054e98813a9f
sorrycc <sorrycc@gmail.com>
2016-02-27 11:30:38 +0800

    use reselector for memorize

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
-import marked from 'marked';
 
 import * as _actions from '../actions/index';
+import appSelectors from '../selectors/appSelectors';
 
 import Stars from '../components/Stars/Stars';
 import Header from '../components/Header/Header';
@@ -44,52 +44,10 @@ class App extends Component {
   }
 }
 
-function getFilteredStars(stars, keyword) {
-  let ret = stars;
-  if (keyword) {
-    ret = ret.filter(item => {
-      return item.name.indexOf(keyword) > -1 ||
-        (item.description && item.description.indexOf(keyword) > -1);
-    });
-  } else {
-    ret = ret.slice(0, 100);
-  }
-  return ret;
-}
-
-function getDetail(_readme, stars) {
-  const { data, selectedStarId } = stars;
-  const star = data.filter(item => item.id === selectedStarId)[0];
-
-  let repo;
-  if (star) {
-    const { name, owner } = star;
-    repo = `${owner.login}/${name}`;
-  }
-
-  let readme;
-  if (repo && _readme[repo]) {
-    readme = marked(atob(_readme[repo]));
-  }
-
-  return { readme, repo };
-}
-
-function mapStateToProps(state) {
-  return {
-    filteredStars: getFilteredStars(state.stars.data, state.uistate.keyword),
-    detail: getDetail(state.readme, state.stars),
-    stars: state.stars,
-    uistate: state.uistate,
-    user: state.user,
-    readme: state.readme,
-  };
-}
-
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(_actions, dispatch),
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(appSelectors, mapDispatchToProps)(App);
---
+++ src/selectors/appSelectors.js
@@ -0,0 +1,63 @@
+import marked from 'marked';
+import { createSelector } from 'reselect';
+
+const starsSelector = state => state.stars;
+const readmeSelector = state => state.readme;
+const uistateSelector = state => state.uistate;
+const userSelector = state => state.user;
+
+const filteredStarsSelector = createSelector([
+  starsSelector,
+  uistateSelector,
+], (stars, uistate) => {
+  const keyword = uistate.keyword;
+  let ret = stars.data;
+  if (keyword) {
+    ret = ret.filter(item => {
+      return item.name.indexOf(keyword) > -1 ||
+        (item.description && item.description.indexOf(keyword) > -1);
+    });
+  } else {
+    ret = ret.slice(0, 100);
+  }
+  return ret;
+});
+
+const detailSelector = createSelector([
+  starsSelector,
+  readmeSelector,
+], (stars, _readme) => {
+  const { data, selectedStarId } = stars;
+  const star = data.filter(item => item.id === selectedStarId)[0];
+
+  let repo;
+  if (star) {
+    const { name, owner } = star;
+    repo = `${owner.login}/${name}`;
+  }
+
+  let readme;
+  if (repo && _readme[repo]) {
+    readme = marked(atob(_readme[repo]));
+  }
+
+  return { readme, repo };
+});
+
+export default createSelector([
+  starsSelector,
+  readmeSelector,
+  uistateSelector,
+  userSelector,
+  filteredStarsSelector,
+  detailSelector,
+], (stars, readme, uistate, user, filteredStars, detail) => {
+  return {
+    stars,
+    readme,
+    uistate,
+    user,
+    filteredStars,
+    detail,
+  };
+});
